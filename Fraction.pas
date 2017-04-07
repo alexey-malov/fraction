5b5378b44c61b04da6eadbe801d0d87457dc07d7
Alexey Malov <alexey.malov@ispringsolutions.com>
2017-04-07 18:30:15 +0300

    Реализовано преобразование и вывод смешанной дроби

--- Fraction.pas
+++ Fraction.pas
@@ -1,10 +1,18 @@
 PROGRAM Fraction(INPUT, OUTPUT);
 VAR
   Numerator, Denominator: INTEGER;
+  IntegerPart: INTEGER;
 
 BEGIN
   READLN(Numerator);
   READLN(Denominator);
 
-  WRITELN(Numerator, '/', Denominator)
+  IntegerPart := Numerator DIV Denominator;
+  Numerator := Numerator MOD Denominator;
+
+  IF IntegerPart = 0
+  THEN
+    WRITELN(Numerator, '/', Denominator)
+  ELSE
+    WRITELN(IntegerPart, ' ', Numerator, '/', Denominator)
 END.
