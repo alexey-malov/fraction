246f82f10ffa4bc09bc195edb189c9dc78376ef2
Alexey Malov <alexey.malov@ispringsolutions.com>
2017-04-07 19:17:59 +0300

    Реализовано упрощение простой дроби

--- Fraction.pas
+++ Fraction.pas
@@ -1,7 +1,6 @@
 PROGRAM Fraction(INPUT, OUTPUT);
 VAR
-  Numerator, Denominator: INTEGER;
-  IntegerPart: INTEGER;
+  Numerator, Denominator, IntegerPart: INTEGER;
 
 {Выводит смешанную дробь на экран}
 PROCEDURE PrintFraction(IntegerPart, Numerator, Denominator: INTEGER);
@@ -13,12 +12,35 @@ BEGIN
     WRITELN(IntegerPart, ' ', Numerator, '/', Denominator)
 END;
 
+{Упрощает правильную дробь (дробь, где числитель меньше знаменателя)}
+PROCEDURE SimplifyFraction(VAR Numerator, Denominator: INTEGER);
+VAR
+  {Greatest Common Denominator - наибольший общий делитель}
+  GCD: INTEGER;
+BEGIN
+  GCD := Numerator;
+  WHILE GCD > 1
+  DO
+    BEGIN
+      IF (Numerator MOD GCD = 0) AND (Denominator MOD GCD = 0)
+      THEN
+        BEGIN
+          Numerator := Numerator DIV GCD;
+          Denominator := Denominator DIV GCD;
+          GCD := Numerator
+        END
+      ELSE
+        GCD := GCD - 1
+    END
+END;
+
 BEGIN
   READLN(Numerator);
   READLN(Denominator);
 
   IntegerPart := Numerator DIV Denominator;
   Numerator := Numerator MOD Denominator;
+  SimplifyFraction(Numerator, Denominator);
 
   PrintFraction(IntegerPart, Numerator, Denominator)
 END.
